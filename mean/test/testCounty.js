c50634f1be5602e2ca2565d448de27b2168dc03e
sonimehul <soni.mehul@octoconsulting.com>
2015-03-21 17:10:48 -0400

    Run Unit Test with details

--- mean/test/testCounty.js
+++ mean/test/testCounty.js
@@ -14,13 +14,13 @@ describe('County Test', function() {
 
 
 	
-	it('should be able to search country and return overall result', function(done) {
+	it('should be able to search country and return results for each domain and overall ', function(done) {
 		request(app).get('/api/search?q=Fairfax County,VA')
 			.end(function(req, res) {
 				// Call the assertion callback
-				//res.body.should.be.an.Array.with.lengthOf(1);
+				//res.body.should.be.an.Array.with.lengthOf(5);
 				var county = res.body;				
-				(county.detail.air.nitro).should.match("0.3");
+				county.state.should.match("VA");
 				done();
 			});
 	});
