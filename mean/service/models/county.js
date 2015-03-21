ffaa06bf24592e83a988821287c74d1ff645f958
Tarandeep Dhupar <me@mydomain.com>
2015-03-20 21:10:05 -0400

    3_20_service

--- mean/service/models/county.js
+++ mean/service/models/county.js
@@ -1,25 +1,8 @@
 var mongoose = require('mongoose');
 
-module.exports = mongoose.model('Opportunity',{
-<<<<<<< HEAD
-=======
-                                opNumber : {type: String, unique: true},
->>>>>>> 1cbc578cce3b7efc753e80248d29dbf578ebf4ad
-                                opTitle : String,
-                                opType : String,
-                                opDesc : String, 
-                                setAside : String,
-                                opStatus : String,
-<<<<<<< HEAD
-                                buyerName : String,
-=======
->>>>>>> 1cbc578cce3b7efc753e80248d29dbf578ebf4ad
-                                publishedDt : Date,
-                                respDueDt : Date,
-                                createdBy : String,
-                                createdDt : {type: Date, default: Date.now},
-                                modifiedBy : String,
-                                modifiedDt : Date,
-                                perfPlace : String,
-                                primaryContact : String
+module.exports = mongoose.model('county',{
+                                stfips : Number,
+                                county_name : String,
+                                state : String,
+                                overall_result : Number
                 });
