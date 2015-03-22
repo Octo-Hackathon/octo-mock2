01e40a11eee50fca13200f130d89067bf2ba947b
Dave Reed <dave@contagious.ly>
2015-03-22 16:01:57 -0400

    Updated with graphs (stub)

--- mean/ui/src/app/views/results/results.js
+++ mean/ui/src/app/views/results/results.js
@@ -29,13 +29,51 @@
 
         function ResultsController($scope, Restangular, countyData, $state) {
             $scope.countyData = countyData;
-            $scope.barData = {
-                labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            $scope.housingData = {"total":104061,"vacancy":{"number":30881,"percent":29},"occupancy":{"number":73180,"percent":70},"rentalOccupancy":{"number":34135,"percent":32},"ownerOccupancy":{"number":53071,"percent":50}};
+            $scope.populationData = {"race":{"white":{"number":156153,"percent":85.67},"black":{"number":17105,"percent":9.38},"americanIndianAlaskaNative":{"number":1216,"percent":0.67},"asian":{"number":1348,"percent":0.74},"nativeHawaiian":{"number":89,"percent":0.05},"otherRace":{"number":3631,"percent":1.99},"twoOrMore":{"number":2723,"percent":1.49}},"sex":{"male":{"number":89196,"percent":48.94},"female":{"number":93069,"percent":51.06}},"total":182265};
+
+            $scope.barData1 = {
+                labels: ['Male', 'Female'],
                 series: [
-                    [5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
-                    [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4]
+                    [$scope.populationData.sex.male.number, $scope.populationData.sex.female.number]
                 ]
             };
+
+            $scope.barData2 = {
+                labels: ['White', 'Black', 'americanIndianAlaskaNative', 'asian', 'nativeHawaiian', 'otherRace', 'twoOrMore'],
+                series: [
+                    [
+                     $scope.populationData.race.white.number,
+                     $scope.populationData.race.black.number,
+                     $scope.populationData.race.americanIndianAlaskaNative.number,
+                     $scope.populationData.race.asian.number,
+                     $scope.populationData.race.nativeHawaiian.number,
+                     $scope.populationData.race.otherRace.number,
+                     $scope.populationData.race.twoOrMore.number
+                    ]
+                ]
+            };
+
+            $scope.barData3 = {
+                labels: ['Homeowners', 'Renters'],
+                series: [
+                    [
+                     $scope.housingData.ownerOccupancy.number,
+                     $scope.housingData.rentalOccupancy.number
+                    ]
+                ]
+            };
+
+            $scope.barData4 = {
+                labels: ['Occupied', 'Vacant'],
+                series: [
+                    [
+                     $scope.housingData.occupancy.number,
+                     $scope.housingData.vacancy.number
+                    ]
+                ]
+            };
+
             $scope.barOptions = {
                 seriesBarDistance: 15
             };
@@ -57,10 +95,7 @@
                     }
                 }]
             ];
-            // pie chart
-            $scope.pieData = {
-                series: [20, 10, 30, 40]
-            };            
+         
         }
 })();
 
