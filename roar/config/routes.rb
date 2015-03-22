09a029e605b17b1897438d83588c95366c834df7
Sreedhar Boggarapu <sreedhar.boggarapu@octoconsulting.com>
2015-03-22 15:22:33 -0400

    commit

--- roar/config/routes.rb
+++ roar/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'migration2', to: 'eqi_details#migrate', path: '/admin/migrate/EQIDetails'
   get 'zip', to: 'zip#show', path: '/admin/getZipInformation/:id(.:json)'
   get 'house', to: 'house#show', path: '/admin/getHousingInfo/:id'
+  get 'house', to: 'population#show', path: '/admin/getPopulationInfo/:id'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
